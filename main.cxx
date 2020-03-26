58f8b0c8b220e6e02a6cd3a093154faaabae5112
aymanrs <54403956+aymanrs@users.noreply.github.com>
2020-03-26 13:47:17 +0100

    removing unnessecary function

--- main.cxx
+++ main.cxx
@@ -1,13 +1,6 @@
 #include <iostream>
 #include <vector>
 
-unsigned long long comb(int k, int n){
-	unsigned long long s = 1;
-	for(int i = (n-k+1);i <= n;i++) s *= i;
-	for(int i = 2;i <= k;i++) s /= i;
-	return s;
-}
-
 int main(){
 	const int seed = 1;
 	std::vector<std::vector<unsigned long long>> triangle = {{0, 0, seed, 0, 0}};
